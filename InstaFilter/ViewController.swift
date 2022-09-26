632a472b9c20408d59f63ee20dc89e5fca957447
Paulo Filho <pcf.feat@gmail.com>
2022-09-26 14:36:08 -0400

    study: attach new bar button item action to open up image picker

--- InstaFilter/ViewController.swift
+++ InstaFilter/ViewController.swift
@@ -7,16 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet var intensity: UISlider!
     @IBOutlet var imageView: UIImageView!
+    var currentImage: UIImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "InstaFilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
-
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
     
     @IBAction func changeFilter(_ sender: Any) {
     }
